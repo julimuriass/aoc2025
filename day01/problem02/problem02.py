c4b29b6cfb047a5d15c20ec74b6f0b1b4cb3fb2f
julimuriass <julieta.murias@gmail.com>
2026-01-11 12:39:04 -0300

    day01-problem2 solved.

--- day01/problem02/problem02.py
+++ day01/problem02/problem02.py
@@ -1,44 +1,74 @@
-FILE_NAME = "/home/julieta-murias/advent-of-code-2025/day01/problem02/testInput.txt"            
+from dataclasses import dataclass
+from typing import List
 
-def get_new_position(pos, action, steps, zeros): 
+FILE_NAME = "/home/julieta-murias/advent-of-code-2025/day01/problem02/input.txt"            
 
-    if action == "L":
+
+    
+def next_pos(current_pos: int, action: str) -> int:
+    dir = action[0:1]
+    steps = int(action[1:])
+    if dir == "L":
         steps = steps * -1
+    new_pos = (current_pos + steps) % 100
 
-    original_pos = pos 
-    pos += steps
+    #new_pos = current_pos + steps
 
-
-    if action == "L": 
+    if new_pos < 0:
+        pass
+    elif new_pos > 99:
         pass
 
+    return new_pos
 
-
-    if pos == 0: 
-        zeros = zeros + 1
-
-    while pos > 99 or pos < 0: 
-        pos = pos % 100
-        if original_pos != 0: 
-            zeros = zeros + 1
-    
-
-    return pos, zeros
-
-def solve_problem():
+def aoc_01_part1_decode(actions: List[str]):
+    count = 0
     pos = 50
-    zeros = 0
+    for action in actions:
+        pos = next_pos(pos, action)
+        if pos == 0:
+            count+=1
+    return count
 
-    with open(FILE_NAME) as f:
-        for line in f:
-            action = line[0:1]
-            steps = int(line[1:])
 
-            pos, zeros = get_new_position(pos, action, steps, zeros)
 
+class DialCounter():
+    def __init__(self):
+        self.current_pos = 50
+        self.zero_count = 0
+
+    def execute_action(self, action: str):
+        dir = action[0:1]
+        steps = int(action[1:])
+        for _ in range(steps):
+            self._move(dir)
+            if self.current_pos == 0:
+                self.zero_count += 1
     
-    print(zeros)
-        
-            
-solve_problem()
+    def _move(self, dir):
+        if dir == "L":
+            self.current_pos -= 1
+            if self.current_pos < 0:
+                self.current_pos = 99
+        else:
+            self.current_pos += 1
+            if self.current_pos > 99:
+                self.current_pos = 0      
 
+
+def aoc_01_part2_decode(actions: List[str]):
+    counter = DialCounter()
+    for action in actions:
+        counter.execute_action(action)
+    return counter.zero_count
+
+
+if __name__ == "__main__":
+
+    # Day 1.
+    with open(FILE_NAME) as f:
+        lines_day_1 = [line.strip() for line in f.readlines()]
+
+    count_1_1 = aoc_01_part1_decode(actions=lines_day_1)
+    count_1_2 = aoc_01_part2_decode(actions=lines_day_1)
+    print(count_1_2)
